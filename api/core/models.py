413f552960276ed6469947226141e4f00689299c
JN513 <jn5.guape@gmail.com>
2020-05-20 10:41:11 -0300

    criando primeira model e arquivo serialize.py

--- api/core/models.py
+++ api/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(db_index=True, max_length=50,unique=True)
+    email = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=0)
+    username = models.CharField(max_length=200, unique=True)
+    datetime = models.DateTimeField(default=datetime.now,blank=True)
+    
+    def __str__(self):
+        return self.nome
